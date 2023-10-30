4dd6c6499b485240830a2e5a8a2911f47d9e0cef
victorvegaward <victor.vega6@upr.edu>
2023-10-30 11:04:26 -0400

    verification for update doctor profile

--- app.py
+++ app.py
@@ -358,8 +358,16 @@ def edit_profile():
                 "payload.schedule.clock_in": clock_in_time,
                 "payload.schedule.clock_out": clock_out_time,
             }
-
             collection = mongo.db.users
+            # Check if email already exists in the database
+            existing_email = collection.find_one({"email": email})
+            if existing_email and existing_email['_id'] != user_id:
+                flash("Email already exists", "error")  # Flash an error message
+                return redirect(request.url)  # Redirect back to the same page
+            existing_phone = collection.find_one({"payload.phone_number": phone_number})
+            if existing_phone and existing_phone['_id'] != user_id:
+                flash("Phone already exists", "error")  # Flash an error message
+                return redirect(request.url)  # Redirect back to the same page
             collection.update_one({"_id": ObjectId(user_id)}, {
                 "$set": updated_payload
             })
