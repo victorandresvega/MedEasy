4205bd57c1642f39b23153457afe4fac3b6763c9
Ricardo Rodriguez Gonzalez <ricardo.rodriguez67@upr.edu>
2023-12-05 20:29:59 -0400

    [CHANGES] Modified Notification Messages:
    - Notification messages are shorter and shown in a specific column.
    - Patient phone number is now in it's own column.
    - Red delete button is shown in the actions column.
    - Modified DateTime format.

--- notification.py
+++ notification.py
@@ -1,6 +1,9 @@
+import locale
 from datetime import datetime
 from bson import ObjectId
 
+# Set the locale to Spanish
+locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
 class Notification:
     def __init__(self, recipient_id, sender_id, sender_name, sender_phone_number, type, appointment_time, message):
         self.recipient_id = recipient_id
@@ -31,18 +34,19 @@ class Notification:
         sender_name = sender_data["payload"]["first_name"] + " " + sender_data["payload"]["last_name"]
         sender_phone_number = sender_data["payload"]["phone_number"]
         appt_time = datetime.fromtimestamp(appointment_time)
+        appt_time_string = appt_time.strftime("%d de %B de %Y %I:%M:%S %p")
         #With all the data that is retrieved, now we create an appropriate message for the notification based 
         #on the type of notification received
         match type:
             case 'Appointment Created':
-                message = "Cita nueva creada por {0} a este tiempo: {1}. El numero de telefono del paciente es {2}.".format(sender_name, appt_time, sender_phone_number)
+                message = "Cita nueva creada por {0}.".format(sender_name)
             case 'Appointment Deleted by Patient':
-                message = "Cita programada para {0} borrada por {1}. El numero de telefono del paciente es {2}.".format(appt_time, sender_name, sender_phone_number)
+                message = "Cita cancelada por {0}.".format( sender_name)
             case 'Appointment Modified by Patient':
-                message = "Cita modificada por {0}. El nuevo tiempo de la cita es {1}. El numero de telefono del paciente es {2}".format(sender_name, appt_time, sender_phone_number)
+                message = "Cita modificada por {0}.".format(sender_name)
             case 'Twenty four hours till appointment':
-                message = "Esto es un recordatorio que usted tiene una cita con {0} a este tiempo: {1}.".format(sender_name, appt_time)
-        notification = Notification(ObjectId(recipient_id), ObjectId(sender_id), sender_name, sender_phone_number, type, appointment_time, message)
+                message = "Recordatorio de su cita con {0}.".format(sender_name)
+        notification = Notification(ObjectId(recipient_id), ObjectId(sender_id), sender_name, sender_phone_number, type, appt_time_string, message)
         notification_document = notification.to_json()
         collection.insert_one(notification_document)
         return notification_document
